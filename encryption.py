388411c4efb998e9a234678237f3396738e7a10f
ashiifi <ashiifi@gmail.com>
2019-05-10 16:04:56 -0500

    Update encryption.py

--- encryption.py
+++ encryption.py
@@ -4,6 +4,7 @@ encryption(S,n) takes in a string and a number. It then moves each letter in the
 by n amount of times through the alphabet. In this way, 'encrypting' the string
 then decryption(W) takes in the encrypted string with the n at the end of it. like "helloworld1"
 """
+
 def encryption(S, n):
 	letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
 	initial = []
@@ -29,7 +30,7 @@ def decryption(W):
 	initial = []
 	decrypted = []
 	num = []
-	for char in W: 
+	for char in W:
 		initial.append(char)
 		a = 0
 		b = 0
@@ -47,10 +48,10 @@ def decryption(W):
 			a = a + 1
 	b = decrypted[:len(decrypted)]
 	d = "".join(b)
-	return d	
+	return d
 
 def main():
-	S = "hello world" 
+	S = "hello world"
 	n = 1
 	#print(encryption("hello world", 1))
 	W = encryption(S, n)
